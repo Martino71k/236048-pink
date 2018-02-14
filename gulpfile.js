7c23c7e7f820a4e0a8a35e63c6242d6837b748c0
Martino <kruchmark@yandex.ru>
2018-02-14 20:19:39 +0300

    Автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,8 @@ var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function() {
   gulp.src("source/less/style.less")
@@ -21,16 +23,16 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -38,7 +40,7 @@ gulp.task("serve", ["style"], function() {
   });
 
 gulp.watch("source/less/**/*.less", ["style"]);
-gulp.watch("source/*.html").on("change", server.reload);
+gulp.watch("source/*.html", ["html"]).on("change", server.reload);
 });
 
 gulp.task("images", function() {
@@ -58,12 +60,12 @@ gulp.task("webp", function() {
 });
 
 gulp.task("sprite", function() {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src("source/img/{icon,logo}-*.svg")
     .pipe(svgstore({
       inlineSvg: true
   }))
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("html", function() {
@@ -71,7 +73,24 @@ gulp.task("html", function() {
     .pipe(posthtml([
       include()
   ]))
-    .pipe(gulp.dest("source"));
+    .pipe(gulp.dest("build"));
 });
 
+gulp.task("copy", function () {
+  return gulp.src ([
+    "source/fonts/**/*.{woof,woof2}",
+    "source/img/**",
+    "source/js/**"
+    ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
 
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("build", function (done) {
+  run("clean", "copy", "style", "sprite", "html", done);
+});
